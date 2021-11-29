7448d3bec1eb8894c9ca31524e48a3b5da17679a
Kinxkay <kayakande7@gmail.com>
2021-11-29 12:09:08 -0500

    Intialization stuff

---
+++ Reversi One source File.cpp
@@ -0,0 +1,485 @@
+
+#include "string"
+#include "iostream"
+#include <list>
+#include <random>
+#include <ctime>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+//#include "char"
+
+
+//REMOVE THESE
+enum class Piece { LIGHT, DARK, EMPTY, BORDER };
+std::ostream& operator<< (std::ostream& stream, const Piece& pieceIn)
+{
+	if (pieceIn == Piece::LIGHT) {
+		stream << "  LIGHT  ";
+	}
+
+	if (pieceIn == Piece::DARK) {
+		stream << "   DARK  ";
+	}
+
+	if (pieceIn == Piece::EMPTY) {
+		stream << "  EMPTY  ";
+	}
+
+	if (pieceIn == Piece::BORDER) {
+		stream << " BOARDER ";
+	}
+	return stream;
+}
+//THESE
+
+
+
+
+
+
+//MOVE.CPP
+typedef int Move;
+typedef const Move NullMove;
+
+
+
+
+
+//BOARD.H
+//enum class Piece { LIGHT, DARK, EMPTY, BORDER };
+const int boardSqaures = 20;
+class Board {
+
+	Piece squares[boardSqaures];
+	std::list <Move> possibleMoves;
+
+public:
+
+	Board();
+
+	void reset();
+
+	void display();
+
+	void makeMove(Piece pieceIn, Move moveIn);
+
+	bool isLegal(Piece pieceIn, Move moveIn);
+
+	Piece getWinner() const;
+
+	Piece getPlayer() const;
+
+	Piece getPiece();
+
+	void genMoves();
+
+	int numMoves() const;
+
+	Move getMove(int, int) const;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+//BOARD.CPP
+Board::Board() {
+	reset();
+}
+
+
+
+void Board::reset() {
+	// int val = 9;
+	//for (int i = 0; i < boardSqaures; i++) {
+	//	bool something = false;
+	//	if (i < 10 || i > 89) {
+	//		squares[i] = Piece::BORDER;
+	//	}
+	//	//0,10,20,30
+
+	//	else if (i%10 == 0) {
+	//		squares[i] = Piece::BORDER;
+	//		something = true;
+	//	}
+	//	
+	//	else if (i == val) {
+	//		squares[i] = Piece::BORDER;
+	//	}
+
+	//	else
+	 for (int i = 0; i < boardSqaures; i++) {
+			bool something = false;
+			if (i < 10 || i > 89) {
+				squares[i] = Piece::BORDER;
+			}
+
+		squares[i] = Piece::EMPTY;
+
+
+		if (something == true) {
+			squares[i -1] = Piece::BORDER;
+		}
+		
+	}
+		
+}
+
+
+
+
+void Board::display() {
+	for (int i = 0; i < boardSqaures; i++) {
+
+		std::cout << "" << i+1 << ":" << squares[i];
+
+		if (i == 9 || i == 19 || i == 29 || i == 39 || i == 49 || i == 59 || i == 69 || i == 79  || i == 89) {
+			std::cout << "" << std::endl;
+		}
+
+	}
+		
+}
+
+void Board::makeMove(Piece pieceIn, Move moveIn) {
+	genMoves();
+
+}
+
+bool Board::isLegal(Piece pieceIn, Move moveIn) {
+	return true;
+}
+
+Piece Board::getWinner() const {
+	return Piece::BORDER;
+}
+
+Piece Board::getPlayer() const {
+	return Piece::BORDER;
+}
+
+Piece Board::getPiece() {
+	return Piece::BORDER;
+}
+
+void Board::genMoves() {
+//	possibleMoves = NULL;
+
+	std::cout << "The number of moves that are currently available are: "<<std::endl;
+
+}
+
+int Board::numMoves() const {
+	return 1;
+}
+
+Move Board::getMove(int, int) const {
+	Move m1 = 3;
+	return m1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//PLAYER.H
+class Player {
+public:
+	std::string  name;
+	Piece playerPiece;
+
+	
+
+	Player(const std::string& nameIn, Piece pieceIn);
+
+	Piece getPiece() const;
+
+	virtual void makeMove(Board& boardIn) = 0;
+
+};
+
+
+
+
+
+
+
+
+
+
+//PLAYER.CPP
+Player::Player(const std::string& nameIn, Piece pieceIn) {
+	this->name = nameIn;
+	this->playerPiece = pieceIn;
+}
+
+Piece Player::getPiece()const {
+	return this->playerPiece;
+}
+
+
+
+
+
+
+
+
+
+//HUMANPLAYER.H
+class HumanPlayer :public Player {
+	HumanPlayer();
+	void makeMove(Board& boardIn);
+};
+
+
+
+
+
+//HUMANPLAYER.CPP
+void HumanPlayer::makeMove(Board& boardIn) {
+
+	/*HumanPlayer::HumanPlayer() :Player("name", Piece::EMPTY) {
+
+	}*/
+
+//	ComputerPlayer::ComputerPlayer(Piece computerPiece) : Player;
+
+	int playerMove;
+	std::cout << "Where do you want to your piece: ";
+	std::cin >> playerMove;
+	std::cout << std::endl;
+
+	boardIn.makeMove(getPiece(), playerMove);
+}
+
+
+
+//COMPUTERPLAYER.H
+ class ComputerPlayer :public Player {
+ public:
+
+	 ComputerPlayer(Piece computerPiece);
+	 void makeMove(Board& boardIn);
+};
+
+
+
+
+
+//COMPUTERPLAYER.CPP
+ ComputerPlayer::ComputerPlayer(Piece computerPiece) :Player("name", Piece::EMPTY) {
+
+//	 Player cp1 = ("Name", Piece::EMPTY);
+
+	static char nameVariation;
+	std::string computerName = "Computer " + nameVariation;
+	Player::name = computerName;
+	Player::playerPiece = computerPiece;
+ }
+
+ void ComputerPlayer::makeMove(Board& boardIn) {
+
+ }
+ 
+
+ 
+
+
+
+
+ class RandomPlayer : public ComputerPlayer {
+	 void makeMove(Board& boardIn);
+ };
+
+
+ void RandomPlayer::makeMove(Board& boardIn) {
+	 Move playerMove;
+	 std::cout << "Where do you want to your piece: ";
+	 std::cin >> playerMove;
+	 std::cout << std::endl;
+
+	 std::random_device device;
+	 std::mt19937 generator(device());
+	 std::uniform_int_distribution<int> distribution(0, 64);
+
+		 std::cout << distribution(device) << std::endl;
+	 
+
+	 boardIn.makeMove(getPiece(), playerMove);
+ }
+
+
+
+
+
+
+
+
+ //GAME.h
+ class Game {
+	 
+	Board gameBoard;
+	HumanPlayer *player1;
+	ComputerPlayer *player2;
+
+ public:
+	 Game();
+	 ~Game();
+	 void selectPlayers();
+	 Player* nextPlayer() const;
+	 bool isRunning() const;
+	 void play();
+	 void announceWinner();
+
+ };
+
+
+ //GAME.CPP
+ Game::Game() {
+	 gameBoard;
+	 player1 =NULL;
+	 player2 = NULL;
+ }
+
+ Game::~Game() {
+	 delete player1;
+	 delete player2;
+ }
+
+ void Game::selectPlayers() {
+	 std::string firstPlayer;
+	 std::string secondPlayer;
+	 std::string playerName;
+
+	 std::cout << "Please enter the type of the first player(human/computer): ";
+	 std::cin >> firstPlayer;
+
+	 if (firstPlayer == "human") {
+		 std::cout << "Please enter the name of the player: ";
+		 std::cin >> playerName;
+	 }
+	 else if (firstPlayer == "computer")
+	 {
+		 //firstPlayer = computer;
+	 }
+
+	 std::cout << "Please enter the second player(human/computer):";
+	 std::cin >> secondPlayer;
+
+	 if (secondPlayer == "human") {
+		 std::cout << "Please enter the name of the player: ";
+		 std::cin >> playerName;
+	 }
+	 else if (secondPlayer == "computer")
+	 {
+		 //secondPlayer = computer;
+	 }
+
+	 //Loop for invalid answers
+	
+	// player1 = new HumanPlayer;
+
+ }
+
+ Player* Game::nextPlayer() const {
+	 return player1;
+ }
+
+ bool Game::isRunning() const {
+	 return true;
+ }
+
+ void Game::play() {
+
+ }
+
+ void Game::announceWinner() {
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//MAIN
+int main() {
+
+	Move NullMove = 200;
+
+	Board myBoard;
+	myBoard.display();
+	
+
+	
+
+
+	/*const Move Nullmove = 100;
+	
+
+	ComputerPlayer cp1(Piece::LIGHT);
+	
+	
+	Board b1;
+	b1.display();*/
+
+
+
+}
+
+//std::ostream& operator<< (std::ostream& stream, const Piece& pieceIn)
+//{
+//	if (pieceIn == Piece::LIGHT) {
+//		stream << "LIGHT";
+//	}
+//
+//	if (pieceIn == Piece::DARK) {
+//		stream << "DARK";
+//	}
+//
+//	if (pieceIn == Piece::EMPTY) {
+//		stream << "EMPTY";
+//	}
+//
+//	if (pieceIn == Piece::BORDER) {
+//		stream << "BOARDER";
+//	}
+//	return stream;
+//}
